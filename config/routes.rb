38bd751f91b8f1fc05a84bb9b4dfe3802406e702
vinsonly <vinsonly@live.ca>
2017-11-02 15:22:02 -0700

    Generated welcome controller, pasted in nav-bar bootstrap code into application.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  match	':controller(/:action(/:id))',	:via	=>	:get
+  match	':controller(/:action(/:id))',	:via	=>	:post
+
+  get 'welcome/index'
+
+  root 'welcome#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
