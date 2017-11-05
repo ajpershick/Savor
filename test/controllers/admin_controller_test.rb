b836abfc98e3889199ea1f927842605d13b57931
jeremyfell <jeremyafell@gmail.com>
2017-11-04 19:50:03 -0700

    Added fixtures, and fixed controller tests

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -3,32 +3,32 @@ require 'test_helper'
 class AdminControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get admin_index_url
-    assert_response :success
+    assert_response :redirect
   end
 
   test "should get show" do
     get admin_show_url
-    assert_response :success
+    assert_response :redirect
   end
 
   test "should get delete" do
     get admin_delete_url
-    assert_response :success
+    assert_response :redirect
   end
 
   test "should get destroy" do
     get admin_destroy_url
-    assert_response :success
+    assert_response :redirect
   end
 
   test "should get new_admin" do
     get admin_new_admin_url
-    assert_response :success
+    assert_response :redirect
   end
 
   test "should get create_admin" do
     get admin_create_admin_url
-    assert_response :success
+    assert_response :redirect
   end
 
 end
