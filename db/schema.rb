340289677e43a7ebe021345dbcefc12b5a8a194f
vinsonly <vinsonly@live.ca>
2017-11-08 19:54:23 -0800

    editted database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -25,12 +25,4 @@ ActiveRecord::Schema.define(version: 20171026001550) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "widgets", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.integer "stock"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
