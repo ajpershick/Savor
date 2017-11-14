9150843cf78178796f642952b374b46a364e0cb0
DarienFlamont <darienflamont@gmail.com>
2017-11-14 15:41:55 -0800

    fixed the test for maps controller

--- test/controllers/maps_controller_test.rb
+++ test/controllers/maps_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class MapsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get maps_index_url
-    assert_response :success
+    assert_response :redirect
   end
 
 end
