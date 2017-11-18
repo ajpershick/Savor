97c406e16a9471a7e0e126a0c5f42000636f97af
jeremyfell <jeremyafell@gmail.com>
2017-11-18 14:28:03 -0800

    Added buttons to change transaction order and view previous and next months

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -24,13 +24,18 @@ class HistoryController < ApplicationController
       @selected_year = params[:year]
     end
 
+    if params[:order].blank? then
+      @order = "latest"
+    else
+      @order = params[:order]
+    end
+
     user = User.find(session[:user_id]) # Get the current logged in user
     transactions = user.transactions.order(:date => :desc) # Get all the user's transactions
     latest_date = transactions[0].date.to_date
     earliest_date = transactions[-1].date.to_date
     # Creates an array of all months between the user's first transaction and latest transaction
     @month_range = (earliest_date..latest_date).map{|d| {year: d.year, month: d.month}}.uniq.reverse
-
     # Gets all month with the selected month
     month_transactions = transactions.where('extract(year from date) = ?', @selected_year).where('extract(month from date) = ?', @selected_month)
 
@@ -49,6 +54,29 @@ class HistoryController < ApplicationController
       transaction_index += 1
     end
 
+    current_index =  @month_range.each_index.detect{|i| @month_range[i][:year].to_s == @selected_year && @month_range[i][:month].to_s == @selected_month}
+
+    puts @month_range[0]
+    puts current_index
+    if current_index == @month_range.length - 1 then
+      @previous_month = nil
+      @previous_year = nil
+    else
+      @previous_month = @month_range[current_index + 1][:month]
+      @previous_year = @month_range[current_index + 1][:year]
+    end
+
+    if current_index == 0 then
+      @next_month = nil
+      @next_year = nil
+    else
+      @next_month = @month_range[current_index - 1][:month]
+      @next_year = @month_range[current_index - 1][:year]
+    end
+
+    if @order == "earliest" then
+      @transaction_days.reverse!
+    end
   end
 
 end
