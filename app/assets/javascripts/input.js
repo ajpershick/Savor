820980eefc2b75238c24e0ac8a08289a315ee5f7
jeremyfell <jeremyafell@gmail.com>
2017-11-19 15:14:00 -0800

    Added table for category icons

--- app/assets/javascripts/input.js
+++ app/assets/javascripts/input.js
@@ -1,3 +1,25 @@
+// Takes any element and automatically lightens up its background color
+function lightenUp(element) {
+  hex = element.dataset.color;
+  red = parseInt((hex[1] + hex[2]), 16);
+  blue = parseInt((hex[3] + hex[4]), 16);
+  green = parseInt((hex[5] + hex[6]), 16);
+
+  all = [red, blue, green];
+  for (var i = 0; i < 3; i ++) {
+    all[i] += 30;
+    if (all[i] > 255) all[i] = 255;
+    all[i] =  all[i].toString(16);
+  }
+
+  element.style.backgroundColor =  "#" + all[0] + all[1] + all[2];
+}
+
+// Returns an elements background color to its initial value
+function revertColor(element) {
+  element.style.backgroundColor = element.dataset.color;
+}
+
 
 function isValidNum(amount) {
   if (amount.length > 8) return false;
