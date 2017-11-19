2ffe96b9cdf5cbbf2c3252423b0e312ba03367e8
jeremyfell <jeremyafell@gmail.com>
2017-11-19 00:25:43 -0800

    Precompiled new history assets

--- db/schema.rb
+++ db/schema.rb
@@ -45,4 +45,12 @@ ActiveRecord::Schema.define(version: 20171112210941) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "widgets", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "stock"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
