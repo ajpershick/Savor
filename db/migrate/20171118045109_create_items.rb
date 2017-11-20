494767c69305bf259a5de66c36329f184025783a
vinsonly <vinsonly@live.ca>
2017-11-20 09:06:29 -0800

    disabled automatic primary key for items migration

--- db/migrate/20171118045109_create_items.rb
+++ db/migrate/20171118045109_create_items.rb
@@ -1,8 +1,8 @@
 class CreateItems < ActiveRecord::Migration[5.1]
   def up
-    create_table :items do |t|
+    create_table :items, id: false do |t|
       t.integer "user_id" # References the user that the item belongs to
-
+      t.string "item_id"
       #item_id is automatically recorded
       t.string "access_token" #used to access product data for an Item
 
