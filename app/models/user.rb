6b06e044077e7f4cd48e46c9b16473fde6175f40
ajpershick <ajpershick@gmail.com>
2017-11-20 23:18:39 -0800

    hopefully fixed testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
   has_secure_password
   has_many :transactions
   has_many :items
---
+++ test/models/user_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+
+  def setup
+    @user = User.new(username: 'savortest', name: 'savor',
+                     email: 'savor@savor.com', password_digest: 'test')
+  end
+
+  test 'valid user' do
+    assert @user.valid?
+  end
+
+  test 'name should be present' do
+    @user.name = '     '
+    assert_not @user.valid?
+  end
+
+  test 'email should be present' do
+    @user.email = '     '
+    assert_not @user.valid?
+  end
+
+  test 'user has the correct info' do
+    assert @user.name = 'savor'
+    assert @user.username = 'savortest'
+    assert @user.email = 'savor@savor.com'
+    assert @user.password_digest = 'test'
+  end
+
+  test 'name isnt a number' do
+    @user.name = 1
+    assert_not @user.valid?
+  end
+
+end
