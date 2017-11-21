6ca6db6ff4c0e515a435f1744ca8f1cbe6041dc2
ajpershick <ajpershick@gmail.com>
2017-11-20 23:11:04 -0800

    add some user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(username: 'savortest', name: 'savor', email: 'savor@savor.com', password_digest:'test', admin: false)
+    @user = User.new(username: 'savortest', name: 'savor',
+                      email: 'savor@savor.com', password_digest: 'test')
   end
 
   test 'valid user' do
@@ -17,6 +18,18 @@ class UserTest < ActiveSupport::TestCase
 
   test 'email should be present' do
     @user.email = '     '
+    assert_not @user.valid?
+  end
+
+  test 'user has the correct info' do
+    assert @user.name = 'savor'
+    assert @user.username = 'savortest'
+    assert @user.email = 'savor@savor.com'
+    assert @user.password_digest = 'test'
+  end
+
+  test 'name isnt a number' do
+    @user.name = 1
     assert_not @user.valid?
   end
 
