8e4f4a66344fa6fe326b7506294ad28e34490ad0
vinsonly <vinsonly@live.ca>
2017-11-26 15:05:23 -0800

    changed unneccessary instance variables in account_controller to local variables

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -20,25 +20,25 @@ class AccountController < ApplicationController
   end
 
   def make_edit
-    @id = params[:userID]
-    user = User.find(@id)
-    @newUsername = params[:username]
-    @newFirstName = params[:first_name]
-    @newEmail = params[:email]
-    @authenticated_user = user.authenticate(params[:password])
+    id = params[:userID]
+    user = User.find(id)
+    newUsername = params[:username]
+    newFirstName = params[:first_name]
+    newEmail = params[:email]
+    authenticated_user = user.authenticate(params[:password])
       #returns user or false
 
-    if((@newUsername.present? && @newFirstName.present? && @newEmail.present? && params[:password].present?) == false)
+    if((newUsername.present? && newFirstName.present? && newEmail.present? && params[:password].present?) == false)
       @message = "Please fill in all fields"
       redirect_to(:action => "edit", :message => @message) and return
-    elsif(@authenticated_user == false)
+    elsif(authenticated_user == false)
       @message = "Password Incorrect"
       redirect_to(:action => "edit", :message => @message) and return
     else
-      toEdit = User.find(@id)
-      toEdit.username = @newUsername
-      toEdit.name = @newFirstName
-      toEdit.email = @newEmail
+      toEdit = User.find(id)
+      toEdit.username = newUsername
+      toEdit.name = newFirstName
+      toEdit.email = newEmail
       toEdit.save
       @message = "Account details editted"
       redirect_to(:action => "index", :message => @message) and return
@@ -48,9 +48,9 @@ class AccountController < ApplicationController
 
   def index
 
-    @id = session[:user_id]
+    id = session[:user_id]
 
-    thisUser = User.find(@id)
+    thisUser = User.find(id)
     @username = thisUser.username
     @first_name = thisUser.name
     @email = thisUser.email
@@ -66,7 +66,7 @@ class AccountController < ApplicationController
 
   def change_password
     @message = params[:message]
-    @authenticated_user = params[:authenticated_user]
+    authenticated_user = params[:authenticated_user]
 
     if (session[:admin])
       render :layout => 'admin'
@@ -77,28 +77,28 @@ class AccountController < ApplicationController
   end
 
   def make_password_change
-    @current = params[:current]
-    @new = params[:new]
-    @confirm = params[:confirm]
-    @id = session[:user_id]
-    user = User.find(@id)
-    @authenticated_user = user.authenticate(params[:current])
+    current = params[:current]
+    new = params[:new]
+    confirm = params[:confirm]
+    id = session[:user_id]
+    user = User.find(id)
+    authenticated_user = user.authenticate(params[:current])
 
     #redirect_to(:action => "change_password", :message => @message, :authenticated_user => @authenticated_user) and return
-    if ((@new.present? && @confirm.present? && @current.present? ) == false)
+    if ((new.present? && confirm.present? && current.present? ) == false)
       @message = "Please fill in all required fields."
-      redirect_to(:action => "change_password", :message => @message, :authenticated_user => @authenticated_user) and return
-    elsif(@authenticated_user == false)
+      redirect_to(:action => "change_password", :message => @message, :authenticated_user => authenticated_user) and return
+    elsif(authenticated_user == false)
       @message = "Please reenter your current password"
-      redirect_to(:action => "change_password", :message => @message, :authenticated_user => @authenticated_user) and return
-    elsif (@new != @confirm)
+      redirect_to(:action => "change_password", :message => @message, :authenticated_user => authenticated_user) and return
+    elsif (new != confirm)
       @message = "Please make sure your new passwords are matching"
-      redirect_to(:action => "change_password", :message => @message, :authenticated_user => @authenticated_user) and return
+      redirect_to(:action => "change_password", :message => @message, :authenticated_user => authenticated_user) and return
     else
-      user.password = @new
+      user.password = new
       user.save
       @message = "Password successfully changed"
-      redirect_to(:action => "index", :message => @message, :authenticated_user => @authenticated_user) and return
+      redirect_to(:action => "index", :message => @message, :authenticated_user => authenticated_user) and return
     end
 
   end
