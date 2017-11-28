5bccde88f986a7fd87673195ef659da932746a05
apershic <apershic@sfu.ca>
2017-11-27 21:25:31 -0800

    added feature/admin_create_user

--- spec/features/admin_creates_user_spec.rb
+++ spec/features/admin_creates_user_spec.rb
@@ -1,10 +1,16 @@
+require "rails_helper"
+
 feature 'User creates a foobar' do
   scenario 'they see the foobar on the page' do
-    visit new_foobar_path
+    visit 'admin/new_user'
 
-    fill_in 'Name', with: 'My foobar'
-    click_button 'Create Foobar'
-
-    expect(page).to have_css '.foobar-name', 'My foobar'
+    fill_in 'Username', with: 'Bobby', :match => :prefer_exact
+    fill_in 'First Name', with: 'Bob', :match => :prefer_exact
+    fill_in 'Email', with: 'Bob@bob.com', :match => :prefer_exact
+    fill_in 'Password', with: 'password', :match => :prefer_exact
+    fill_in 'confirm_pass', with: 'password', visible: false
+    click_button 'Submit', :match => :prefer_exact
+    response.should redirect_to 'admin/index'
+    expect(page).to have_css 'Bob'
   end
 end
