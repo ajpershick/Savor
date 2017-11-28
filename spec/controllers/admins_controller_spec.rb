7768e87d94887622e959576732c48396120e9418
apershic <apershic@sfu.ca>
2017-11-27 17:19:47 -0800

    successfully log in as admin

--- spec/controllers/admins_controller_spec.rb
+++ spec/controllers/admins_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 require 'spec_helper'
 
-
 RSpec.describe AdminController, type: :controller do
 
   let(:valid_attributes) {
@@ -25,6 +24,17 @@ RSpec.describe AdminController, type: :controller do
       session[:admin].should be_truthy
       @user.admin.should eq(true)
       get :index
+      response.should be_success
+    end
+  end
+
+
+  describe "get new_user" do
+    it "returns a success response" do
+      @user.name.should eq('Admin')
+      session[:admin].should be_truthy
+      @user.admin.should eq(true)
+      get new_user_path
       response.should be_success
     end
   end
