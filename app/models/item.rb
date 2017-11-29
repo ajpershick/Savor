0d529a985dba72840f1e44db12d5162f1f411fb0
ajpershick <ajpershick@gmail.com>
2017-11-29 15:20:20 -0800

    finish items model/spec

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :access_token, presence: true
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should belong_to(:user)}
+
+  it { should validate_presence_of :user_id }
+  it { should validate_presence_of :access_token }
+
+  it "should have valid factory" do
+    FactoryGirl.build(:random_item).should be_valid
+  end
 end
