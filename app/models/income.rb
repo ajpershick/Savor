fe49c70da10564721ffc3a36907a17bb0568499a
ajpershick <ajpershick@gmail.com>
2017-11-29 15:25:08 -0800

    add validations to income model

--- app/models/income.rb
+++ app/models/income.rb
@@ -1,3 +1,9 @@
 class Income < ApplicationRecord
+
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :access_token, presence: true
+  validates :income_amount, presence: true
+  validates :source, presence: true
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
+
   belongs_to :user
 
   validates :user_id, presence: true
-  validates :access_token, presence: true
+  validates :income_amount, presence: true
 end
