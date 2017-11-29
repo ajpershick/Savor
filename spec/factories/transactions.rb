d7ebe30d12a8511a0f07a0b35b84117f95d8dfa9
ajpershick <ajpershick@gmail.com>
2017-11-29 00:45:56 -0800

    finish transaction factory and model-spec

---
+++ spec/factories/transactions.rb
@@ -0,0 +1,15 @@
+FactoryGirl.define do
+  sequence(:user_id) { |n| n }
+
+  factory :random_transaction do
+    user_id {FactoryGirl.generate :user_id}
+    amount {Faker::Number.number}
+    date {Faker::Date.forward}
+    category {Faker::Team.state}
+    transaction_type {Faker::Superhero.prefix}
+    location {Faker::University.name}
+    latitude {Faker::Address.latitude}
+    longitude {Faker::Address.longitude}
+  end
+end
+
