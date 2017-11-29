48e9997b713d55ffc171ee45a8f29cb22ecd3074
ajpershick <ajpershick@gmail.com>
2017-11-28 18:28:56 -0800

    finally figured out logging in as admin, added feature/creating_users_spec

--- spec/features/creating_users_spec.rb
+++ spec/features/creating_users_spec.rb
@@ -1,21 +1,24 @@
-# require 'rails_helper'
-#
-# feature "get new_user" do
-#   it "normal user can't get new_index" do
-#     @admin = build(:random_admin)
-#     login(@admin)
-#     login_admin
-#     page.set_rack_session(user: @admin.id)
-#     page.set_rack_session(admin: true)
-#     visit '/admin/new_user'
-#     expect(page).to have_current_path('admin/new_user')
-#     fill_in 'Username', with: 'Bobby', :match => :prefer_exact
-#     fill_in 'First Name', with: 'Bob', :match => :prefer_exact
-#     fill_in 'Email', with: 'Bob@bob.com', :match => :prefer_exact
-#     fill_in 'Password', with: 'password', :match => :prefer_exact
-#     fill_in 'confirm_pass', with: 'password', visible: false
-#     click_button 'Submit', :match => :prefer_exact
-#     expect(page).to have_current_path(new_user_path, url: true)
-#     expect(page).to have_css 'Bob'
-#   end
-# end
+require 'rails_helper'
+
+feature "get new_user" do
+  it "Creates a new user and displays them in admin/index" do
+    @admin = create(:random_admin)
+    # login(@admin)
+    # login_admin
+    ApplicationController.any_instance.stub(:confirm_admin_logged_in)
+    # page.set_rack_session(user: @admin.id)
+    # page.set_rack_session(admin: true)
+    visit 'admin/new_user'
+    expect(page).to have_current_path('/admin/new_user')
+    within('#create_account') do
+      fill_in 'Username', with: 'Bobby', :match => :prefer_exact
+      fill_in 'First Name', with: 'Bob', :match => :prefer_exact
+      fill_in 'Email', with: 'Bob@bob.com', :match => :prefer_exact
+      fill_in 'Password', with: 'password', :match => :prefer_exact
+      fill_in 'confirm_pass', with: 'password', visible: false
+      click_button 'Submit', :match => :prefer_exact
+    end
+    expect(page).to have_current_path('/admin/index')
+    expect(page).to have_selector("td", text: @admin.username)
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,6 +34,47 @@ RSpec.configure do |config|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
+  config.before(:suite) do
+    if config.use_transactional_fixtures?
+      raise(<<-MSG)
+        Delete line `config.use_transactional_fixtures = true` from rails_helper.rb
+        (or set it to false) to prevent uncommitted transactions being used in
+        JavaScript-dependent specs.
+
+        During testing, the app-under-test that the browser driver connects to
+        uses a different database connection to the database connection used by
+        the spec. The app's database connection would not be able to access
+        uncommitted transaction data setup over the spec's database connection.
+      MSG
+    end
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, type: :feature) do
+    # :rack_test driver's Rack app under test shares database connection
+    # with the specs, so continue to use transaction strategy for speed.
+    driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
+
+    if !driver_shares_db_connection_with_specs
+      # Driver is probably for an external browser with an app
+      # under test that does *not* share a database connection with the
+      # specs, so use truncation strategy.
+      DatabaseCleaner.strategy = :truncation
+    end
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.append_after(:each) do
+    DatabaseCleaner.clean
+  end
+
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
@@ -101,5 +142,7 @@ RSpec.configure do |config|
 =end
   config.include Capybara::DSL
   config.include Capybara::RSpecMatchers
+
+
 end
 
