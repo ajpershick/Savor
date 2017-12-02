d6186ca50e50a01caf367e7dae97385b12b5dbd1
ajpershick <ajpershick@gmail.com>
2017-12-01 17:15:03 -0800

    deleted test directory

--- test/models/account_balance_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AccountBalanceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/bank_transaction_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class BankTransactionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
