ceb1971967f6033a049eaf24d6a2dfc73da42d5b
DarienFlamont <darienflamont@gmail.com>
2017-12-01 16:14:32 -0800

    cleaned up maps controller pass to view

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -7,41 +7,30 @@ class MapsController < ApplicationController
   def index
     mapuser = User.find(session[:user_id])
 
-    #grab latitude and longitude and store them in variables as well as address
-    lat = mapuser.transactions.latitude.order(:date => :desc)
-    long = mapuser.transactions.longitude.order(:date => :desc)
+    if mapuser.transactions.location == true
+      #grab latitude and longitude and store them in variables as well as address
+      lat = mapuser.transactions.latitude
+      long = mapuser.transactions.longitude
 
-    if lat.length == 0 then
-      @empty = true
-      return
-    end
-    @empty = false
+      if lat.length == 0 then
+        @empty = true
+        return
+      end
+      @empty = false
 
-    if long.length == 0 then
-      @empty = true
-      return
-    end
-    @empty = false
+      if long.length == 0 then
+        @empty = true
+        return
+      end
+      @empty = false
 
-    latest_lat = lat[0].date.to_date
-    earliest_lat = lat[-1].date.to_date
-    latest_long = long[0].date.to_date
-    earliest_long = long[-1].date.to_date
 
-    class Latlongarray
       def initialize(lat,long)
-      @latlongArr = Array.new(lat) {Array.new(long)}
+        @latlongArr = Array.new(lat) {Array.new(long)}
       end
 
-
-
-    if (mapuser.transactions.location == true) then
-
-      return view('viewlatlongArr')->with(@latlongArr)
-      return view('viewlocationArr')->with(@locationArr)
+        return view('viewlatlongArr')->with(@latlongArr)
 
     end
-
-
   end
 end
