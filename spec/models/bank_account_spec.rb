68eccc37650c2d7c46ced04b417f28f2c3ccbb58
ajpershick <ajpershick@gmail.com>
2017-12-01 23:34:12 -0800

    added stuff to bankaccount model

--- spec/models/bank_account_spec.rb
+++ spec/models/bank_account_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe BankAccount, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should belong_to(:item) }
+  it { should have_many(:bank_transactions) }
+
+  it { should validate_presence_of :user_id }
+  it { should validate_presence_of :amount }
+  it { should validate_presence_of :date }
+  it { should validate_presence_of :category }
+  it { should validate_presence_of :transaction_type }
+  it { should validate_presence_of :location }
+  it { should validate_presence_of :latitude }
+  it { should validate_presence_of :longitude }
+
+  it "should have valid factory" do
+    FactoryGirl.build(:random_transaction).should be_valid
+  end
 end
