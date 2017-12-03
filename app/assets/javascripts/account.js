7311d9a8f3dfe16a6594eff0c28822395703541a
jeremyfell <jeremyafell@gmail.com>
2017-12-03 15:02:03 -0800

    Fixed issues with edit account verification

--- app/assets/javascripts/account.js
+++ app/assets/javascripts/account.js
@@ -1,10 +1,7 @@
 function verify_edit(username) {
   var check = 4;
 
-  if (
-    validateInput(document.getElementById("entry-input-username"), "username") ||
-    document.getElementById("entry-input-username").value === username
-  ) check--;
+  if (validateExisting(document.getElementById("entry-input-username"), username)) check--;
   if (validateInput(document.getElementById("entry-input-name"), "name")) check--;
   if (validateInput(document.getElementById("entry-input-email"), "email")) check--;
   if (validateInput(document.getElementById("entry-input-password"), "password")) check--;
@@ -35,4 +32,37 @@ function verify_edit(username) {
 
     return false;
   }
+}
+
+// Validate the input 1 second after the user has finished typing
+function delayedValidateExisting(item, username) {
+  delay(function() {
+    validateExisting(item, username);
+  }, 1000)
+}
+
+// Validate a username in the edit account page
+function validateExisting(item, username) {
+
+  input = item.value;
+
+  // When there is no input, make the input box the default gray
+  if (input.length === 0) {
+    item.parentNode.childNodes[1].className = "entry-icon-container";
+    return;
+  }
+
+  if (document.getElementById("entry-input-username").value === username) {
+    validInput = true;
+  } else {
+    validInput = validateUsername(input);
+  }
+
+  // Makes the input box green or red depending on if the input is valid
+  if (validInput) {
+    item.parentNode.childNodes[1].className = "entry-icon-container entry-valid";
+  } else {
+    item.parentNode.childNodes[1].className = "entry-icon-container entry-invalid";
+  }
+
 }
