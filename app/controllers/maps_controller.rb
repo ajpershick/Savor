ce1613c6cc5911a7292a5956d71f24dec7fc0962
jeremyfell <jeremyafell@gmail.com>
2017-12-02 20:19:07 -0800

    Added functions to automatically center and zoom the map

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -41,11 +41,30 @@ class MapsController < ApplicationController
       current_transactions = transactions.where('extract(year from date) = ?', @selected_year).where('extract(month from date) = ?', @selected_month)
     end
 
+    if !@empty then
+      @max_lat = current_transactions[0].latitude.to_f
+      @min_lat = current_transactions[0].latitude.to_f
+      @max_long = current_transactions[0].longitude.to_f
+      @min_long = current_transactions[0].longitude.to_f
+    end
+
     @locations = []
     current_transactions.each do |transaction|
-      lat = transaction.latitude
-      long = transaction.longitude
+      lat = transaction.latitude.to_f
+      long = transaction.longitude.to_f
       @locations << [lat, long]
+
+      if lat > @max_lat then
+        @max_lat = lat
+      elsif lat < @min_lat then
+        @min_lat = lat
+      end
+
+      if long > @max_long then
+        @max_long = long
+      elsif long < @min_long then
+        @min_long = long
+      end
     end
 
     #
