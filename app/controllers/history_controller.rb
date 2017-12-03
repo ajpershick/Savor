0cc95daea6ea951fd4b5b32c73c565f4a8db6094
jeremyfell <jeremyafell@gmail.com>
2017-12-02 19:41:27 -0800

    Removed deprecated error messages

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -43,7 +43,6 @@ class HistoryController < ApplicationController
     user = User.find(session[:user_id])
     # Get all the user's transactions
     transactions = user.transactions.order(:date => :desc)
-    puts transactions.length
 
     if transactions.length == 0 then
       @empty = true
